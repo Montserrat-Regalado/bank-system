c5363f08fa44e77ec0de38838b34a262b20d8c1d
Montserrat Regalado <montserratregalado@gmail.com>
2023-03-10 00:08:02 -0600

    creation of customer class

--- src/classes/customers/customers.js
+++ src/classes/customers/customers.js
@@ -1,24 +1,87 @@
-class Customers{
-    constructor(name,address,cardNumber,pin){
-        this.name=name;
-        this.address=address;
-        this.cardNumber=cardNumber;
-        this.pin=pin;
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url";
+class Customers {
+    constructor(name, address, cardNumber, pin) {
+        this.name = name;
+        this.address = address;
+        this.cardNumber = cardNumber;
+        this.pin = pin;
 
     }
 
-createCustomers(){
-    const newCustomer=new Map([1,{name:"Jose Carlos"}])
-console.log(newCustomer);
+    readDB(){
+        const _filename=fileURLToPath(import.meta.url);
+        const _dirname=path.dirname(_filename);
+        const pathDB=path.join(_dirname,'../../../DB/customer.db.json')
+        if (fs.existsSync(pathDB)) {
+            const listDb = JSON.parse(fs.readFileSync(pathDB, { encoding: 'utf-8' })) 
+            return listDb
+        }
+    }
+    writeDb(newArray) {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+        const pathDB = path.join(__dirname, '../../../DB/customer.db.json')
+        if (fs.existsSync(pathDB)) {
+            fs.writeFileSync(pathDB,JSON.stringify(newArray))
+        }
+   }
+    createCustomers() {
+        const newCustomer = {[this.cardNumber]: { name: this.name ,address:this.address,pin:this.pin}};
+        const listCustomer=this.readDB()
+        
+        this.writeDb(newCustomer)
+    }
 
-}
+    // getCustomer(cardNumber){
+    //     const customersList= this.readDB()
+
+    //     for (const key in customersList) {
+    //         if (Object.hasOwnProperty.call(customersList, key)) {
+    //             const element = customersList[key];
+    //             console.log(element.name=="Jose Carlos Gonzalez")
+    //             break;               
+    //         }
+    //     }
+    // }
 
 
-//metodo ṕara verificar password
+    //metodo ṕara verificar password
 
-verifyPassword(){
+    //verifyPassword(1234,6552672123123)
+    verifyPassword(pin,numbercard) {
+        const listCustomer=this.readDB();
+        const customer=listCustomer[numbercard] //objeto || undefined
+        //esta sintaxis valida las tres opciones (undefined,null,false)
+        if(!customer){
+            return 'Tarjeta no existe'
+        }
+        if(customer.pin!=pin){
+            return 'PIN inválido'
+        }
 
-}
+        return 'Todo correcto'
+
+        //////////////////////////////////////////////
+        // if(customer){
+        //     if(customer.pin==pin){
+        //         return 'valido'
+        //         if(fechanacioment){
+        //             si es
+        //         }else{
+        //             no no es
+        //         }
+        //     }else {
+        //         return 'pin incorrecto'
+        //     }
+        // }else {
+        //     return 'no existe'
+        // }
+
+        
+        
+    }
 
 }
 export default Customers;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import Customers from "./classes/customers/customers.js";
+
+const x = new Customers("Jose Carlos Gonzalez", "MariaA 225", "12345", "4321")
+
+x.createCustomers()
+console.log(x.verifyPassword("432", "12345"))
