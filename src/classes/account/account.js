79e9249384c2aef62b54bc310aad97397f156919
Montserrat Regalado <montserratregalado@gmail.com>
2023-03-14 21:29:55 -0600

    creacion de account

--- src/classes/account/account.js
+++ src/classes/account/account.js
@@ -1,8 +1,57 @@
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url";
+
 class Account{
     constructor(number,balance){
         this.number=number;
         this.balance=balance
     }
+
+    readDB(){
+        const _filename=fileURLToPath(import.meta.url);
+        const _dirname=path.dirname(_filename);
+        const pathDB=path.join(_dirname,'../../../DB/account.db.json')
+        if (fs.existsSync(pathDB)) {
+            const listDb = JSON.parse(fs.readFileSync(pathDB, { encoding: 'utf-8' })) 
+            return listDb
+        }
+    }
+    writeDb(newArray) {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+        const pathDB = path.join(__dirname, '../../../DB/account.db.json')
+        if (fs.existsSync(pathDB)) {
+            fs.writeFileSync(pathDB,JSON.stringify(newArray))
+        }
+   }
+
+   createCustomers(){
+    const newAccount={[this.number]:{balance:this.balance}};
+    const listAccount=this.readDB()
+
+    this.writeDb(newAccount)
+   }
+
+   deposit(amountToSave,number){
+    const accountlist=this.readDB();
+    const account=accountlist[number];
+    console.log(account)
+    console.log(accountlist)
+
+    if(account===undefined){
+        return 'account doesnot exist'
+    }
+
+    //0
+    account.balance+=amountToSave
+    
+    this.writeDb({[number]:account,...accountlist})
+    
+
+   }
+
+
 }
 
 export default Account ;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import Account from "./classes/account/account.js";
 import Customers from "./classes/customers/customers.js";
 
 const x = new Customers("Jose Carlos Gonzalez", "MariaA 225", "12345", "4321")
 
 x.createCustomers()
-console.log(x.verifyPassword("432", "12345"))
+console.log(x.verifyPassword("4321", "12345"))
+
+const account=  new Account("12345",0)
+console.log(account.deposit(500,"12345"))
